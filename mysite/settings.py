6748b9a5dce9cb733d8c972483f53f7295dcb5f5
oscarrocket <tihankinkirill@gmail.com>
2022-07-29 18:16:31 +0500

    Simple Django Page

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8#*s%e^9e1*b#^7rsrclp1-9q()0i4p85g_&p-y_8oz%%2oqo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
